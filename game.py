492be94b20595bba68d84c9b9e45a6abae4cb786
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-09-08 10:32:21 +0700

    :lipstick: Edited game's colour

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ SCREEN_HEIGHT = 600
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
-        arcade.set_background_color(arcade.color.BLACK)
+        arcade.set_background_color((55,71,79))
         self.ship = arcade.Sprite('assets/images/rocket.png')
         self.ship.set_position(100, 100)
 
