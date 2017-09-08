5f7f583c31e0436e964fe5a6fb476af4b451006c
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-09-08 10:30:01 +0700

    :truck: Edited image's location

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         arcade.set_background_color(arcade.color.BLACK)
-        self.ship = arcade.Sprite('images/ship.png')
+        self.ship = arcade.Sprite('assets/images/rocket.png')
         self.ship.set_position(100, 100)
 
     def on_draw(self):
