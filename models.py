c848e021f2bfe4c94dfcb7273647b9c1dcdc911a
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-09-08 10:40:00 +0700

    :art: Moved rocket to another class.

---
+++ models.py
@@ -0,0 +1,10 @@
+from ship import Ship
+class Ship:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def update(self, delta):
+        if self.y > 600:
+            self.y = 0
+        self.y += 5
