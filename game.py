8dca9252c45a9132f464b3229f94a3f7046856eb
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-09-08 11:00:29 +0700

    :lipstick: Move world to another class

--- game.py
+++ game.py
@@ -1,28 +1,37 @@
 import arcade
-from models import Ship
+import arcade.key
+from models import World, Ship
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+        super().__init__(*args, **kwargs)
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
 
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         arcade.set_background_color((55,71,79))
-        self.ship = Ship(100,100)
-        self.ship_sprite = arcade.Sprite('assets/images/rocket.png')
+        self.world = World(width,height)
+        self.ship_sprite = ModelSprite('assets/images/rocket.png',model=self.world.ship)
     def on_draw(self):
         arcade.start_render()
         self.ship_sprite.draw()
     def update(self, delta):
-        ship = self.ship
-        ship.update(delta)
-        self.ship_sprite.set_position(ship.x, ship.y)
+        self.world.update(delta)
 
 class Ship:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
     def update(self, delta):
         if self.y > 600:
             self.y = 0
