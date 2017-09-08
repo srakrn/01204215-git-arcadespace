851a8baadf559cd25eda54f970f1b2ddca68fbca
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-09-08 10:34:38 +0700

    ::construction: Rocket move in progress

--- game.py
+++ game.py
@@ -14,6 +14,12 @@ class SpaceGameWindow(arcade.Window):
         arcade.start_render()
         self.ship.draw()
 
+    def update(self, delta):
+        ship = self.ship
+        if ship.center_y > SCREEN_HEIGHT:
+            ship.center_y = 0
+        ship.set_position(ship.center_x, ship.center_y + 5)
+
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run()
